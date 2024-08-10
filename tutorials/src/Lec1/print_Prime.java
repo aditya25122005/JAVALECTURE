f8cac06696b173138ab70eafa888e730ac8afdf6
SUPER-150 <aditya25pda@gmail.com>
2024-08-10 19:19:27 +0530

    new commit

---
+++ tutorials/src/Lec1/print_Prime.java
@@ -0,0 +1,11 @@
+package Lec1;
+
+public class print_Prime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ALL PRIME NO UPTO N");
+
+	}
+
+}
